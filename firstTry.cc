9f59b4789f4df19ea02cc5191f927e37114961aa
Programming-NMCOTyet <162403457+Programming-NMCOTyet@users.noreply.github.com>
2024-06-17 16:00:26 -0500

    Update firstTry.cc

--- firstTry.cc
+++ firstTry.cc
@@ -3,26 +3,23 @@
 using namespace std;
 
 struct viaje{
-
     char nombreviaje[50];
     char destino[50];
-    int precio = 0, capacidad = 0, dia = 0, mes = 0, year = 0;
+    int precio = 0, capacidad = 0;
     int altura, nmatricula=0, IdViaje=0;
 
     viaje *izq, *der; 
 }; 
 
-viaje *raiz, *aux2;
+viaje *raiz, *aux2, *aux;
 
 struct pasajero {
-
     char nombrepsj[50];
     int doc = 0;
     pasajero* sig;
-   
 };
 
-pasajero *aux1, *cab,*cab2, *aux ,*aux4;
+pasajero *aux1, *cab;
 
 int obtenerAltura(viaje *n) {
     if (n == NULL) {
@@ -74,15 +71,10 @@ viaje* insertar(viaje* nodo) {
     }
 
     if (aux2->IdViaje < nodo->IdViaje) {
-
         nodo->izq = insertar(nodo->izq);
-
     } else if (aux2->IdViaje > nodo->IdViaje) {
-
         nodo->der = insertar(nodo->der);
-
     } else {
-
         return nodo;
     }
 
@@ -112,354 +104,269 @@ viaje* insertar(viaje* nodo) {
 }
 
 int inorden(viaje *nodo) {
-
     if (nodo != NULL) {
         inorden(nodo->izq);
         cout << nodo->IdViaje << " ";
         inorden(nodo->der);
     }
-
     return 0;
 }
 
+int obtenerFecha() {
+    int year, mes, dia;
+    cout << "por favor registre la fecha actual: " << endl;
+    cout << "Ingrese el año (4 dígitos): ";
+    cin >> year;
+    cout << "Ingrese el mes (1-12): ";
+    cin >> mes;
+    cout << "Ingrese el día (1-31): ";
+    cin >> dia;
+
+    if (mes < 1 || mes > 12 || dia < 1 || dia > 31) {
+        cout << "Fecha no válida. Inténtelo nuevamente." << endl;
+        return obtenerFecha();
+    }
+
+    int fecha = year * 10000 + mes * 100 + dia;
+    return fecha;
+}
+
 int registrar() {
-
     aux2 = (struct viaje*) malloc(sizeof(struct viaje));
-
-
-    
     cout << "Ingrese el valor de su viaje: "; 
     cin >> aux2->precio;
-
     cout << "Ingrese el destino de la embarcacion: "; 
     cin >> aux2->destino; 
-    cin.ignore(1000, '\n');
-
     cout << "Ingrese el numero de matricula de la embarcacion: "; 
     cin >> aux2->nmatricula;
-
     cout << "Ingrese el nombre de la embarcacion: "; 
     cin >> aux2->nombreviaje; 
-    cin.ignore(1000, '\n');
- 
-    cout << "Ingrese el año de registro: "; 
-    cin >> aux2->year;
-
-    cout << "Ingrese el mes de registro: "; 
-    cin >> aux2->mes;
-
-    cout << "Ingrese el dia de registro: "; 
-    cin >> aux2->dia;
-
+    aux2->IdViaje = obtenerFecha();
     cout << "Ingrese el numero de pasajeros que se embarcaran: "; 
     cin >> aux2->capacidad;
-
-    int Idunico = aux2->nmatricula / 10000;
-    aux2->IdViaje = Idunico * 100000000 + aux2->year * 10000 + aux2->mes * 100 + aux2->dia;
-
     aux2->izq = NULL; 
     aux2->der = NULL; 
     aux2->altura = 1;
     raiz = insertar(raiz);
-
     return 0;
 }
 
-int listarViaje(viaje* viaje_actual){
-
-    if(viaje_actual !=NULL){
-
-        
-    cout << "INFORMACION DE REGISTROS: " << endl << endl;
-
-
-    cout << "Precio: " << aux2->precio << endl;
-    cout << "Destino: " << aux2->destino << endl;
-    cout << "MATRICULA: " << aux2->nmatricula << endl;
-    cout << "Nombre de la embarcacion: " << aux2->nombreviaje << endl;
-    cout << "Fcha de registro: AÑO " << aux2->year << ", MES " << aux2->mes << ", DIA " << aux2->dia << endl;
-    cout << "ID del viaje: " << aux2->IdViaje << endl ;
-    cout << "Capacidad de viajeros: " << aux2->capacidad << endl << endl;
-
+int listarViaje(viaje* viaje_actual) {
+    if(viaje_actual != NULL) {
+        cout << "INFORMACION DE REGISTROS: " << endl << endl;
+        cout << "Precio: " << aux2->precio << endl;
+        cout << "Destino: " << aux2->destino << endl;
+        cout << "MATRICULA: " << aux2->nmatricula << endl;
+        cout << "Nombre de la embarcacion: " << aux2->nombreviaje << endl;
+        cout << "ID del viaje: " << aux2->IdViaje << endl;
+        cout << "Capacidad de viajeros: " << aux2->capacidad << endl << endl;
         listarViaje(viaje_actual->izq);
         listarViaje(viaje_actual->der);
-
     }  
     return 0;
 }
 
-int buscarViaje(){ 
-    
-    int Buscar;
-    cout<<"Digite el numero de id que desea buscar: "; cin>>Buscar;
-    viaje* aux3= raiz;
-
-    while(aux3 != NULL){
-
-        if(aux3->IdViaje == Buscar){
-
-
-        cout << "INFORMACION DE REGISTROS ENCONTRADOS: " << endl << endl;
-
-       
-    cout << "Precio: " << aux2->precio << endl;
-    cout << "Destino: " << aux2->destino << endl;
-    cout << "MATRICULA: " << aux2->nmatricula << endl;
-    cout << "Nombre de la embarcacion: " << aux2->nombreviaje << endl;
-    cout << "Fcha de registro: AÑO " << aux2->year << ", MES " << aux2->mes << ", DIA " << aux2->dia << endl;
-    cout << "ID del viaje: " << aux2->IdViaje << endl ;
-    cout << "Capacidad de viajeros: " << aux2->capacidad << endl << endl;
-
-
-         return 1;
-
-        } else if(aux3->IdViaje < Buscar){
-
-            aux3 = aux3->izq;
-
-        } else{
-
-            aux3 = aux3->der;
-
-        }
+viaje* buscarIdViaje(viaje* nodo, int idBusqueda) {
+    if (nodo == NULL || nodo->IdViaje == idBusqueda) {
+        return nodo;
     }
     
-    cout<<"Viaje no encontrado: "<<endl;
+    if (idBusqueda < nodo->IdViaje) {
+        return buscarIdViaje(nodo->izq, idBusqueda);
+    } else {
+        return buscarIdViaje(nodo->der, idBusqueda);
+    }
+}
 
+
+
+
+int buscarYMostrarIdViaje(viaje* nodo, int idBusqueda) {
+    viaje* resultado = buscarIdViaje(nodo, idBusqueda);
+    if (resultado != NULL) {
+        cout << "Viaje encontrado: " << endl;
+        cout << "Precio: " << resultado->precio << endl;
+        cout << "Destino: " << resultado->destino << endl;
+        cout << "MATRICULA: " << resultado->nmatricula << endl;
+        cout << "Nombre de la embarcacion: " << resultado->nombreviaje << endl;
+        cout << "ID del viaje: " << resultado->IdViaje << endl;
+        cout << "Capacidad de viajeros: " << resultado->capacidad << endl << endl;
+        return 1;
+    } else {
+        cout << "Viaje no encontrado." << endl;
+        return 0;
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+int ubicar(viaje *aux3, int aguja) {
+    if(aux3->IdViaje == aguja) {
+        aux = aux3;
+        return 0;
+    } else {
+        if(aux3->izq != NULL) {
+            ubicar(aux3->izq, aguja);
+        }
+        if(aux3->der != NULL) {
+            ubicar(aux3->der, aguja);
+        }
+    }
     return 0;
 }
 
-viaje* valor_minimo(viaje* nodo){
-
-    viaje* actual = nodo;
-
-    while (actual && actual->izq != NULL)
-    actual = actual->izq;
-
-    return actual;
+//ubicar padre
+int ubicarpadre(viaje *padre) {
+    if((padre->izq != NULL) && (padre->izq != aux)) {
+        ubicarpadre(padre->izq);
+    }
+    if(padre->izq == aux) {
+        aux2 = padre;
+    }
+    if((padre->der != NULL) && (padre->der != aux)) {
+        ubicarpadre(padre->der);
+    }
+    if(padre->der == aux) {
+        aux2 = padre;
+    }
+    return 0;
 }
 
-viaje* eliminar_viaje(viaje* nodo, int Eliminar) {
-
-    if (nodo == NULL)
-
-        return nodo;
-
-    if (Eliminar < nodo->IdViaje)
-
-        nodo->izq = eliminar_viaje(nodo->izq, Eliminar);
-
-    else if (Eliminar > nodo->IdViaje)
-
-        nodo->der = eliminar_viaje(nodo->der, Eliminar);
-
-    else {
-
-        if (nodo->izq == NULL || nodo->der == NULL) {
-
-            viaje* temp = nodo->izq ? nodo->izq : nodo->der;
-
-            if (temp == NULL) {
-                temp = nodo;
-                nodo = NULL;
-            } else
-                *nodo = *temp;
-            free(temp);
-
-        } else {
-            viaje* temp = valor_minimo(nodo->der);
-            nodo->IdViaje = temp->IdViaje;
-            nodo->der = eliminar_viaje(nodo->der, temp->IdViaje);
-        }
+//casos de eliminacion 
+int casouno() {
+    if(aux != raiz) {
+        ubicarpadre(raiz);
     }
-
-    if (nodo == NULL)
-        return nodo;
-
-    nodo->altura = 1 + mayor(obtenerAltura(nodo->izq), obtenerAltura(nodo->der));
-
-    int balance = obtenerBalance(nodo);
-
-    if (balance > 1 && obtenerBalance(nodo->izq) >= 0)
-        return rotarDerecha(nodo);
-
-    if (balance > 1 && obtenerBalance(nodo->izq) < 0) {
-        nodo->izq = rotarIzquierda(nodo->izq);
-        return rotarDerecha(nodo);
+    if(aux2->izq == aux) {
+       aux2->izq = NULL;
+    } else if(aux2->der == aux) {
+        aux2->der = NULL;
     }
-
-    if (balance < -1 && obtenerBalance(nodo->der) <= 0)
-        return rotarIzquierda(nodo);
-
-    if (balance < -1 && obtenerBalance(nodo->der) > 0) {
-        nodo->der = rotarDerecha(nodo->der);
-        return rotarIzquierda(nodo);
-    }
-
-    return nodo;
+    free(aux);
+    return 0;
 }
 
-int eliminar_registro(){
-    int borrar;
-    cout<<"INGRESE EL ID DEL VIAJE QUE QUIERE ELIMINAR: "<<endl;
-    cin>>borrar;
+int casodos() {
+    if(aux != raiz) {
+       ubicarpadre(raiz);
+    }
+    if(aux2->izq == aux) {
+        if (aux->izq != NULL)
+            aux2->izq = aux->izq;
+        else if (aux->der != NULL)
+            aux2->izq = aux->der;
+    } else if (aux2->der == aux) {
+        if (aux->izq != NULL)
+            aux2->der = aux->izq;
+        else if (aux->der != NULL)
+            aux2->der = aux->der;
+    }
+    free(aux);
+    return 0;
+}
 
-    viaje* nodoActual = raiz;
-    viaje* padre = NULL;
-
-    while (nodoActual != NULL) {
-        if (borrar == nodoActual->IdViaje) {
-            if (nodoActual->izq == NULL || nodoActual->der == NULL) {
-                viaje* temp = nodoActual->izq ? nodoActual->izq : nodoActual->der;
-
-                if (padre == NULL) {
-                    raiz = temp;
-                    
-                } else if (padre->izq == nodoActual) {
-                    padre->izq = temp;
-                } else {
-                    padre->der = temp;
-                }
-
-                free(nodoActual);
-
-            } else {
-
-                viaje* temp = valor_minimo(nodoActual->der);
-                nodoActual->IdViaje = temp->IdViaje;
-                nodoActual->der = eliminar_viaje(nodoActual->der, temp->IdViaje);
-            }
-
-            cout << " Viaje eliminado: ";
-            return 0;
-
-        } else if (borrar < nodoActual->IdViaje) {
-            padre = nodoActual;
-            nodoActual = nodoActual->izq;
-        } else {
-            padre = nodoActual;
-            nodoActual = nodoActual->der;
-        }
-    }    return 0;
+int casotres() {
+    if(aux != raiz) {
+       ubicarpadre(raiz);
+    }
+    if(aux2->der == aux) {
+        if (aux->der != NULL)
+            aux2->der = aux->der;
+        else if (aux->izq != NULL)
+            aux2->der = aux->izq;
+    } else if (aux2->izq == aux) {
+        if (aux->der != NULL)
+            aux2->izq = aux->der;
+        else if (aux->izq != NULL)
+            aux2->izq = aux->izq;
+    }
+    free(aux);
+return 0;
 }
 
 
- int registrarPasajero(){
+int eliminar() {
+    int buscar;
+    cout << "Ingrese el valor del codigo que desea eliminar..." << endl;
+    cin >> buscar;
 
-        if(cab2==NULL){
-      
-        cab2 = (struct  pasajero* )malloc(sizeof(struct  pasajero));
+    ubicar(raiz, buscar);
 
-        cout << "Ingrese el nombre del pasajero: ";
-        cin >> cab2->nombrepsj;
-
-        cout << "Ingrese el documento del pasajero: ";
-        cin >> cab2->doc;
-        
-        cab2->sig = NULL;
+    if (aux == NULL) {
+        cout << "El viaje con ID " << buscar << " no fue encontrado." << endl;
         return 0;
-
-
-        
-        }else{
-
-       
-        aux = (struct  pasajero* )malloc(sizeof(struct  pasajero));
-
-        
-        cout << "Ingrese el nombre del pasajero: ";
-        cin >> aux->nombrepsj;
-
-        cout << "Ingrese el documento del pasajero: ";
-        cin >> aux->doc;
-          
-        
-        aux->sig = NULL;
-        aux4 = cab;
-
-     while(aux4->sig !=NULL)
-            aux4 = aux4->sig;
-        
-        aux4->sig = aux;
-
-        aux4 = aux = NULL;
-
-        free (aux);
-        free(aux4);
-          
-     }
-     
-     return 0;
     }
 
+    if (aux->der == NULL && aux->izq == NULL) {
+        cout << "Eliminando..." << endl;
+        casouno();
+    } else if (aux->der == NULL || aux->izq == NULL) {
+        cout << "Eliminando..." << endl;
+        casodos();
+    } else {
+        cout << "Eliminando..." << endl;
+        casotres();
+    }
 
-int listarPasajeros() {
-   
-
-        for(aux = cab2; aux; aux = aux->sig){ 
-
-        cout << "INFORMACION DE REGISTROS: " << endl << endl;
-
-        cout << "El nombre del pasajero es: ";
-        cin >> aux->nombrepsj;
-
-        cout << "El documento del pasajero es: ";
-        cin >> aux->doc;
-
-
-        }
-        return 0;
-
-
-   
+ return 0;
 }
-
-
-
-
 
 int main() {
-
     int op = 0;
-    
-    do{
-        cout<<"MENU PRINCIPAL: "<<endl;
-        
-        cout<<"1. REGISTRAR VIAJE"<<endl;
-        cout<<"2. LISTADO DE VIAJES"<<endl;
-        cout<<"3. BUSCAR VIAJE"<<endl;
-        cout<<"4. ELIMINAR VIAJE"<<endl;
-        cout<<"5. REGISTRAR PASAJERO A UN VIAJE"<<endl;
-        cout<<"6. LISTADO DE PASAJEROS DEL VIAJE"<<endl;
-        cout<<"7. SALIR"<<endl << endl;
+    do {
+        cout << "MENU PRINCIPAL: " << endl;
+        cout << "1. REGISTRAR VIAJE" << endl;
+        cout << "2. LISTADO DE VIAJES" << endl;
+        cout << "3. BUSCAR VIAJE" << endl;
+        cout << "4. ELIMINAR VIAJE" << endl;
+        cout << "5. REGISTRAR PASAJERO A UN VIAJE" << endl;
+        cout << "6. LISTADO DE PASAJEROS DEL VIAJE" << endl;
+        cout << "7. SALIR" << endl << endl;
         cin >> op;
 
-        switch (op){   
-            
+        switch (op) {   
             case 1:
-            registrar();
-            break;
-
+                registrar();
+                break;
             case 2: 
-            listarViaje(raiz); 
-            break;
-
-            case 3: 
-            buscarViaje(); 
-            break;
+                listarViaje(raiz); 
+                break;
+                
+            case 3: {
+                int idBuscar;
+                cout << "Ingrese el ID del viaje que desea buscar: ";
+                cin >> idBuscar;
+                int encontrado = buscarYMostrarIdViaje(raiz, idBuscar);
+                if (encontrado) {
+                    cout << "Búsqueda exitosa." << endl;
+                } else {
+                    cout << "No se encontró ningún viaje con el ID " << idBuscar << "." << endl;
+                }
+                break;
+            }
 
             case 4: 
-            eliminar_registro(); 
-            break;
+                eliminar(); 
+                break;
 
+                
+                
+                
             case 5: 
-           
-            break;
-
+                // Registrar pasajero
+                break;
             case 6: 
-           
+                // Listar pasajeros
             break;
-
+            
             case 7: 
             cout<< "Saliendo......." <<endl; 
             break;
@@ -468,5 +375,5 @@ int main() {
             break;
         }
     } while (op!=7);
-  return 0;
+    return 0;
 }
